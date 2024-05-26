5c625cca53ba6b9db79d1ddea48e9a8d6738417b
Deepak <ideepakrabindra@gmail.com>
2024-05-27 04:01:42 +0530

    - feat: Updated onRequest function to handle HttpMethod.post - refactor: Converted _getUsers and _createUser functions to async functions - chore: Added async/await to improve code readability and maintainability

--- routes/user/index.dart
+++ routes/user/index.dart
@@ -1,13 +1,14 @@
 import 'package:dart_frog/dart_frog.dart';
 
-Response onRequest(RequestContext context) {
+Future<Response> onRequest(RequestContext context) async {
   return switch (context.request.method) {
     HttpMethod.get => _getUsers(),
-    _ => Response(body: 'Nothing to show')
+    HttpMethod.post => _createUser(context),
+    _ => Future.value(Response(body: 'Nothing to show'))
   };
 }
 
-Response _getUsers() {
+Future<Response> _getUsers() async {
   return Response.json(
     body: [
       {
@@ -19,5 +20,23 @@ Response _getUsers() {
         'lastName': 'Kumar',
       },
     ],
+  );
+}
+
+Future<Response> _createUser(RequestContext context) async {
+  final json = (await context.request.json()) as Map<String, dynamic>;
+  final name = json['name'];
+  final lastName = json['lastName'];
+
+  // TODO :  Insert user information into DB
+
+  return Response.json(
+    body: {
+      'message': 'User Created!',
+      'user': {
+        'name': name,
+        'lastName': lastName,
+      },
+    },
   );
 }
